de3f4f25271c7cd572eef61dc54157cd63e76eb8
Chindurex <chinnaracha@gmail.com>
2026-09-14 11:32:55 +0700

    refactor: update project structure and naming conventions

---
+++ src/main/java/com/FSD_11/Pet_Sitter/PetSitterApplication.java
@@ -0,0 +1,13 @@
+package com.FSD_11.Pet_Sitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FSD_11/Pet_Sitter/PetSitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FSD_11.Pet_Sitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
